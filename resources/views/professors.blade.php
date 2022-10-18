19b2d0eef2c67956838a872b43c27dc2e831f909
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2022-10-17 22:07:17 -0300

    Update professors.blade.php

--- resources/views/professors.blade.php
+++ resources/views/professors.blade.php
@@ -6,17 +6,6 @@
 
 <div class="h1 pb-3 mb-4 text-dark border-bottom border-2 border-dark">Professores</div>
 
-<div class="card">
-  <div class="card-header">
-    Cadastro
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Cadastrar Novo Professor</h5>
-    <p class="card-text">Click no botão abaixo para cadastrar um novo aluno na lista de alunos</p>
-    <a href="/professors/create" class="btn btn-dark">Cadastrar</a>
-  </div>
-</div>
-
 <table class="table table-striped">
     <thead>
         <tr>
